e027c1549fd9209eda341f3e5d3ee81db984b5c7
sohalj <sohalj@students.wwu.edu>
2017-12-06 18:39:26 -0800

    changed html layout and set a default for image and quote

--- main.js
+++ main.js
@@ -4,7 +4,26 @@
 
 
 $(document).ready(function(){
+	$.getJSON("quotes.json", function(json){   /*Sets defaults for quote and picture associated with it*/
+				html2="";
+				html3="";
+				
+				html2 += "<div class='quote'>"
+				html2 += "<p>" + '"' + json[0].quoteText + '"' + "</p>"
+				html2 += "<p class='text-right'>" + "- " + json[0].quoteAuthor  + "</p>"
+				html3 += "<img src=" + json[0].imageLink + "alt=" +  json[0].quoteAuthor  + "align=" + "middle" + ">";
+				$(".default").html(html2);
+				$(".defaultpic").html(html3);
+	});
+
+
+
+
+
 	$("#getQuote").on("click", function(){
+		$(".default").remove();
+		$(".defaultpic").remove();
+
 		$.getJSON("quotes.json", function(json){
 			var html = "";
 			var html1 = "";
@@ -25,26 +44,28 @@ $(document).ready(function(){
 			console.log("random number asdfasdf "  + random);
 			var val = json[random];
 
-				html += "<div class = 'quote'>";
+				html += "<div class='quote'>"
+
 
 				html += "<p>" + '"' + val.quoteText + '"' + "</p>"
 
-				html +="</div>";
 
-				html += "<div class = 'author'>";
+				html += "<p class='text-right'>" + "- " + val.quoteAuthor  + "</p>"
 
-				html += "<p>" + "- " + val.quoteAuthor  + "</p>"
+				html += "<button type='button' class='btn btn-primary' id='getQuote'> 'newQuote' </button>"
 
-				html +="</div>";
 
-				html1 += "<img src=" + val.imageLink + "alt=" +  val.quoteAuthor  + ">";
+				html +="</div>"
+
+				html1 += "<img src=" + val.imageLink + "alt=" +  val.quoteAuthor  + "align=" + "middle" + ">";
 		$(".message").html(html);
 		$(".picture").html(html1);
 
 		
-			$(".quote, .author").css({"background-color": "white", 
-																"font-size": "200%",
-																"color": "black"});
+			$(".quote").css({"background-color": "white", 
+																"font-size": "2em",
+																"color": "black",
+																"font-style": "italic"});
 			
 			$("body").css({"background-color": "red"});
 			//$('head').append('<link rel="stylesheet" href="mystyle.css" type="text/css" />');
